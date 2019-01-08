67d79ca35c63738a649fc5f25f07d562f3f963f6
Chris <stac1221@gmail.com>
2019-01-08 00:04:31 -0600

    redesigned home page. added components and styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <div class="header">
+    <div class="top-bar">
+      <div class="container">
+        <div class="d-flex justify-content-end">
+          <a href="#" class="mr-4">@</a>
+          <a href="#" class="mr-4">f</a>
+          <a href="tel:651-688-2358">651-688-2358</a>
+        </div>
+      </div>
+    </div>
     <div class="container">
       <nav class="d-flex align-items-center justify-content-between">
         <div class="logo">
@@ -17,7 +26,7 @@
         </div>
       </nav>
     </div>
-    <div class="border-top"></div>
+    <div class="header-border"></div>
   </div>
 </template>
 
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,13 +3,21 @@
     <div class="hero d-flex align-items-center">
       <div class="filter"></div>
       <div class="container">
-        <h1 class="hero-heading">DG Welding<br><span class="fz-xl italic gray-30">and Mfg.</span></h1>
+          <h1 class="hero-heading d-flex flex-column">
+            <span class="heading-a mb-2">DG Welding</span>
+            <span class="heading-b fz-xl italic gray-30">and Mfg.</span>
+          </h1>
       </div>
     </div>
-    <section class="py-8 bg-pattern">
+
+    <CarouselOurServices />
+
+    <div class="border-top"></div>
+
+    <section class="py-8">
       <div class="container text-center">
-        <h2 class="mb-4">Core Values</h2>
-        <ul class="fz-lg bg-white d-inline-block border p-5">
+        <h2 class="mb-5 pseudo-underline">Core Values</h2>
+        <ul class="fz-lg d-inline-block mb-3">
           <li>Exceed expectations</li>
           <li>Go the extra mile</li>
           <li>Exemplify integrity</li>
@@ -17,66 +25,50 @@
         </ul>
       </div>
     </section>
+
     <div class="border-top"></div>
-    <section class="position-relative offset-images py-6">
-      <!-- <img src="../assets/images/fence.jpg" alt="fence" class="img-left"> -->
+
+    <section class="py-8 bg-pattern">
       <div class="container">
-        <div class="row mb-8">
-          <div class="col-6">
-            <img src="../assets/images/fence.jpg" alt="fence" class="">
-          </div>
-          <div class="col-6 d-flex align-items-center">
-            <div class="content ml-5 fz-lg">
-              <p>DG Welding &amp; Mfg. is a private, family owned welding and steel fabrication shop based in Eagan, Minnesota.</p>
-              <p>With over 40 years of service to the Twin Cities and surrounding areas, we provide structural steel, miscellaneous and ornamental welding and manufacturing on site and in house.</p>
-            </div>
-          </div>
-        </div>
         <div class="row">
-          <div class="col-6 d-flex align-items-center">
-            <div class="content mr-5 fz-lg">
-              <p>Our range of skills and experience in the construction industry has provided us with the knowledge to aid in any project steel related.</p>
-              <p>With no job too big or too small, DG offers a variety of services to fit most diverse contracting, residential or custom needs.</p>
+          <div class="col-md-6">
+            <div class="bg-white p-4 border">
+              <h2 class="mb-5">Contact</h2>
+              <h4 class="mb-4">Located right off of 13 and Yankee Doodle in Eagan, MN</h4>
+              <div class="d-flex flex-column">
+                <a href="#" class="mb-2">&#9873; 3265 Sun Drive, Eagan MN 55121</a>
+                <a href="#" class="mb-2">&#9743; 651-688-2358</a>
+                <a href="mailto:info@dg-welding.com">@ info@dg-welding</a>
+              </div>
             </div>
           </div>
-          <div class="col-6">
-            <img src="../assets/images/custom-corkscrew.jpg" alt="custom corkscrew" class="">
+          <div class="col-md-6">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2829.1007871711613!2d-93.19545428446335!3d44.83988077909854!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62e615e1139cf%3A0x420018bf24b6be85!2s3265+Sun+Dr%2C+Eagan%2C+MN+55121!5e0!3m2!1sen!2sus!4v1546926209770" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
           </div>
-        </div>
+        </div>        
       </div>
-      <!-- <img src="../assets/images/custom-corkscrew.jpg" alt="custom corkscrew" class="img-right"> -->
     </section>
+
   </div>
 </template>
 
 <script>
+import CarouselOurServices from './CarouselOurServices';
+
 export default {
   name: "HomePage",
+  components: {
+    CarouselOurServices
+  },
   methods: {
-    offsetImageSizing() {
-      const offsetImagesEl = document.querySelector('.offset-images');
-      const row1El = document.querySelector('.row-1');
-      const row2El = document.querySelector('.row-2');
-
-      let row1ImgHt = document.querySelector('.img-left').clientHeight;
-      let row1ImgHtString = row1ImgHt + 'px';
-      row1El.style.height = row1ImgHt + 'px';
-
-      let row2ImgHt = document.querySelector('.img-right').clientHeight;
-      let row2ImgHtString = row2ImgHt + 'px';
-      row2El.style.height = row2ImgHt + 'px';
-
-      let offsetImagesHt = row1ImgHt + row2ImgHt;
-      let offsetImagesHtString = offsetImagesHt + 'px';
-      offsetImagesEl.style.height = offsetImagesHtString;
+    shiftHeading() {
+      const heroHeading = document.querySelector('.hero-heading');
+      heroHeading.style.left = '0px';
     }
   },
   mounted() {
     window.addEventListener('load', () => {
-      this.offsetImageSizing();
-    });
-    window.addEventListener('resize', () => {
-      this.offsetImageSizing();
+      this.shiftHeading();
     });
   }
 }
